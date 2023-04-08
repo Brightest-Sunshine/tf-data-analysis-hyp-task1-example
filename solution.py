5b7ad785cde23ecf7fc0618c9f3a990064c78d49
Mamaeva Anastasiia <60839846+Brightest-Sunshine@users.noreply.github.com>
2023-04-09 00:05:59 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,21 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import binomtest
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 453878141 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
-             x_cnt: int, 
+             x_cnt : int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha=0.04
+    p_control_robot = x_success / x_cnt
+    p_control_operator = (x_cnt - x_success) / x_cnt
+    successes_robot = y_success
+    successes_operator = y_success - (y_cnt - y_success) * (p_control_operator / p_control_robot)
+    p_value = binomtest(successes_robot, n=y_cnt, p=p_control_operator, alternative='less').pvalue
+    if p_value > alpha:
+        return True
+    else:
+        return False
